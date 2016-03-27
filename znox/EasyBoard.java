7fc0f2bbb3988c667f8f2a530c8865d8a4c6da12
zNoX_ <tybeatscode@gmail.com>
2016-03-27 14:10:04 -0300

    Update EasyBoard.java

--- znox/EasyBoard.java
+++ znox/EasyBoard.java
@@ -22,7 +22,7 @@ public abstract class EasyBoard {
     public EasyBoard(Player player, String display) {
         this.player = player;
         scoreboard = Bukkit.getServer().getScoreboardManager().getNewScoreboard();
-        objective = scoreboard.registerNewObjective(player.getName() + "scoreboard", "dummy");
+        objective = scoreboard.registerNewObjective("s" + player.getName().toUpperCase(), "dummy");
         objective.setDisplayName(display);
         objective.setDisplaySlot(DisplaySlot.SIDEBAR);
         scoreboards.put(player, this);
