805006cf73833fe97a6561353d31803f77f15f5e
zNoX_ <tybeatscode@gmail.com>
2016-03-27 11:48:39 -0300

    Create Example.java

---
+++ znox/Example.java
@@ -0,0 +1,29 @@
+package znox;
+
+import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
+
+public class Example {
+
+    public void example(Player player) {
+        final EasyBoard easyboard = new EasyBoard(player, "Titulo") {
+            // Coloque tudo que voce quer atualizar aqui!!
+            @Override
+            public void update() {
+                setText(new String[] {
+                        "Texto 1", // Linha 3
+                        "Texto 2", // Linha 2
+                        "Texto 3" // Linha 3
+                });
+            }
+        };
+
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                easyboard.update();
+            }
+        }.runTaskTimer(classePrincipal, 0L, 0L); // Deixe em 0L, 0L para atualizar muito rapido!
+    }
+
+}
